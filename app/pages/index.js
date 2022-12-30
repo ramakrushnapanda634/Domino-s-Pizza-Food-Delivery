eafb000f70ef0de96615d0be4068d3439938fa8f
ramakrushnapanda634 <ramakrushnapanda634@gmail.com>
2022-12-30 21:57:02 +0530

    first

---
+++ app/pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '../styles/Home.module.css'
+import Featured from "../components/Featured"
+import PizzaList from '../components/PizzaList'
+
+// const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+     
+      <Head>
+        <title>Pizza Resturant in Bangalore</title>
+        <meta name="description" content="Best Pizza Shop In Town" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    
+   <Featured/>
+   <PizzaList/>
+    </div>
+  )
+}
